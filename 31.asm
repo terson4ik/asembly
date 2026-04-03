5c9d9e654ad5acb06a329730dde2902666c8d3d1
Vladislav <vladislavsultan846@gmail.com>
2026-04-03 18:50:36 +0300

    31

--- 31.asm
+++ 31.asm
@@ -1,12 +1,23 @@
 %include 'stud_io.inc'
 global _start
+;%define WORD
+;%define DOUBLE
+;%define QUADRO
 
 %macro putarrey 1
     %strlen sl %1
     %assign i 1
     %rep sl
         %substr var %1 i
-        dd var
+        %ifdef WORD
+            dw var
+        %elifdef DOUBLE
+            dd var
+        %elifdef QUADRO
+            dq var
+        %else
+            db var
+        %endif
         %assign i i+1
     %endrep
 %endmacro
