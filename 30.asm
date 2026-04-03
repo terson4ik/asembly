00a70967a78e9fee5296d55641ec1a1683eb66e4
Vladislav <vladislavsultan846@gmail.com>
2026-04-03 18:34:21 +0300

    30

--- 30.asm
+++ 30.asm
@@ -1,8 +1,19 @@
 %include 'stud_io.inc'
 global _start
 
+%macro putarrey 1
+    %strlen sl %1
+    %assign i 1
+    %rep sl
+        %substr var %1 i
+        dd var
+        %assign i i+1
+    %endrep
+%endmacro
+
+section .data
+data    putarrey "Vladislav takoy vot 111)))"
 
 section .text
 _start: 
-
         FINISH
